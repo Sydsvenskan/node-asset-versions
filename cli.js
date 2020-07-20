ef76bb791003ce5a40db73bf2b8b6c5fd67a9d3d
Pelle Wessman <pelle.wessman@hdsydsvenskan.se>
2020-07-20 15:38:46 +0200

    Fix types in cli.js

--- cli.js
+++ cli.js
@@ -5,8 +5,17 @@
 
 'use strict';
 
+const pathModule = require('path');
+
+const cpFile = require('cp-file');
 const debug = require('debug')('asset-versions');
 const dashdash = require('dashdash');
+const loadJsonFile = require('load-json-file');
+const revFile = require('rev-file');
+const VError = require('verror');
+const writeJsonFile = require('write-json-file');
+
+// *** CLI setup ***
 
 const options = [
   {
@@ -39,6 +48,15 @@ const options = [
 
 const parser = dashdash.createParser({ options });
 
+/**
+ * @typedef CommandLineOptions
+ * @property {boolean} [help]
+ * @property {string} [output]
+ * @property {string} [path]
+ * @property {string[]} [_args]
+ */
+
+/** @type {CommandLineOptions} */
 let opts;
 
 try {
@@ -64,84 +82,132 @@ if (opts.help) {
 
 delete opts._args;
 
-const pathModule = require('path');
-
-const cpFile = require('cp-file');
-const revFile = require('rev-file');
-const loadJsonFile = require('load-json-file');
-const writeJsonFile = require('write-json-file');
-const VError = require('verror');
-
-const { objectPromiseAll } = require('@hdsydsvenskan/utils');
+// *** Tool setup ***
 
 const { loadWebpackVersions } = require('./utils/webpack');
 
 const workingDir = opts.path || process.cwd();
 const outputFile = pathModule.resolve(workingDir, opts.output);
-/** @type {Object<string,any>} */
-const assetsOptions = loadJsonFile.sync(pathModule.resolve(workingDir, 'assets.json'));
 
-const { files, sourceDir, targetDir, webpackManifest } = assetsOptions;
+/**
+ * @typedef AssetsOptions
+ * @property {string[]} files
+ * @property {string[]} webpackManifest
+ * @property {string} sourceDir
+ * @property {string} targetDir
+ */
 
-const dependencies = {};
+/**
+ * @param {any} data
+ * @param {string} label
+ * @returns {string[]}
+ */
+const resolveJsonStringArray = (data, label) => {
+  if (!data) return [];
+  if (typeof data === 'string') return [data];
 
-const resolvedSourceDir = pathModule.resolve(workingDir, sourceDir);
-const webpackFiles = webpackManifest ? loadWebpackVersions(resolvedSourceDir, webpackManifest) : {};
+  /** @type {string[]} */
+  const resolvedArray = [];
 
-debug('working dir: %s', workingDir);
-debug('resolved source dir: %s', resolvedSourceDir);
-
-// Add all webpack files as well
-Object.keys(webpackFiles).forEach(file => {
-  if (!file.endsWith('.map') && !files.includes(file)) {
-    files.push(file);
-  }
-});
-
-objectPromiseAll(files.reduce((result, file) => {
-  const sourcePath = pathModule.resolve(resolvedSourceDir, file);
-  const webpackFileRaw = webpackFiles[file];
-
-  debug('file "%s" has source path: %s', file, sourcePath);
-
-  /** @type {string|undefined} */
-  let webpackFile;
-  /** @type {string[]|undefined} */
-  let webpackFileSiblings;
-
-  if (typeof webpackFileRaw === 'string') {
-    webpackFile = webpackFileRaw;
-  } else if (typeof webpackFileRaw === 'object') {
-    webpackFile = webpackFileRaw.path;
-    webpackFileSiblings = webpackFileRaw.siblings;
+  if (!Array.isArray(data)) {
+    throw new TypeError(`Expected a ${label} array, instead got: ${typeof data}`);
   }
 
-  // Has WebPack revved this for us already? Use that file then
-  const webpackRevvedSourcePath = webpackFile
-    ? pathModule.resolve(resolvedSourceDir, webpackFile)
-    : undefined;
+  for (const value of data) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`Expected ${label} array to only contain strings, encountered a:  ${typeof value}`);
+    }
+    resolvedArray.push(value);
+  }
 
-  debug('file "%s" has webpack path: %s', file, webpackRevvedSourcePath);
+  return resolvedArray;
+};
 
-  dependencies[file] = webpackFileSiblings;
+/**
+ * @param {string} workingDir
+ * @returns {Promise<AssetsOptions>}
+ */
+const loadAssetsOptions = async (workingDir) => {
+  const { files, webpackManifest, sourceDir, targetDir } = await loadJsonFile(pathModule.resolve(workingDir, 'assets.json'));
 
-  result[file] = Promise.resolve(webpackRevvedSourcePath || revFile(sourcePath))
-    .catch(err => { throw new VError(err, 'Failed to rev file'); })
-    .then(async target => {
-      const targetFile = pathModule.relative(resolvedSourceDir, target);
-      const targetPath = pathModule.resolve(workingDir, targetDir, targetFile);
+  if (!sourceDir || typeof sourceDir !== 'string') throw new TypeError('Expected a non-empty string sourceDir');
+  if (!targetDir || typeof targetDir !== 'string') throw new TypeError('Expected a non-empty string targetDir');
 
-      await cpFile(webpackRevvedSourcePath || sourcePath, targetPath);
+  return {
+    files: resolveJsonStringArray(files, 'files'),
+    webpackManifest: resolveJsonStringArray(webpackManifest, 'webpackManifest'),
+    sourceDir,
+    targetDir,
+  };
+};
 
-      return pathModule.relative(workingDir, targetPath);
-    })
-    .catch(err => { throw new VError(err, 'Failed to copy file'); });
+loadAssetsOptions(workingDir).then(async ({ files, sourceDir, targetDir, webpackManifest }) => {
+  /** @type {{ [file: string]: string[]|undefined }} */
+  const dependencies = {};
+  /** @type {{ [file: string]: string }} */
+  const copiedFiles = {};
 
-  return result;
-}, {}))
-  // @ts-ignore
-  .then(files => writeJsonFile(outputFile, { files, dependencies }))
-  .catch(err => {
+  const resolvedSourceDir = pathModule.resolve(workingDir, sourceDir);
+  const webpackFiles = webpackManifest.length !== 0 ? loadWebpackVersions(resolvedSourceDir, webpackManifest) : {};
+
+  debug('working dir: %s', workingDir);
+  debug('resolved source dir: %s', resolvedSourceDir);
+
+  // Add all webpack files as well
+  Object.keys(webpackFiles).forEach(file => {
+    if (!file.endsWith('.map') && !files.includes(file)) {
+      files.push(file);
+    }
+  });
+
+  await Promise.all(files.map(async (file) => {
+    const sourcePath = pathModule.resolve(resolvedSourceDir, file);
+    const webpackFileRaw = webpackFiles[file];
+
+    debug('file "%s" has source path: %s', file, sourcePath);
+
+    /** @type {string|undefined} */
+    let webpackFile;
+    /** @type {string[]|undefined} */
+    let webpackFileSiblings;
+
+    if (typeof webpackFileRaw === 'string') {
+      webpackFile = webpackFileRaw;
+    } else if (typeof webpackFileRaw === 'object') {
+      webpackFile = webpackFileRaw.path;
+      webpackFileSiblings = webpackFileRaw.siblings;
+    }
+
+    // Has WebPack revved this for us already? Use that file then
+    const webpackRevvedSourcePath = webpackFile
+      ? pathModule.resolve(resolvedSourceDir, webpackFile)
+      : undefined;
+
+    debug('file "%s" has webpack path: %s', file, webpackRevvedSourcePath);
+
+    dependencies[file] = webpackFileSiblings;
+    // Ensures a consistent order, assigns a string to stick to the type
+    copiedFiles[file] = '';
+
+    return Promise.resolve(webpackRevvedSourcePath || revFile(sourcePath))
+      .catch(/** @param {Error} err */ err => { throw new VError(err, 'Failed to rev file'); })
+      .then(async target => {
+        const targetFile = pathModule.relative(resolvedSourceDir, target);
+        const targetPath = pathModule.resolve(workingDir, targetDir, targetFile);
+
+        await cpFile(webpackRevvedSourcePath || sourcePath, targetPath);
+
+        copiedFiles[file] = pathModule.relative(workingDir, targetPath);
+      })
+      .catch(/** @param {Error} err */ err => { throw new VError(err, 'Failed to copy file'); });
+  }));
+
+  await writeJsonFile(outputFile, {
+    files: copiedFiles,
+    dependencies
+  });
+})
+  .catch(/** @param {Error} err */ err => {
     // eslint-disable-next-line no-console
     console.error('Encountered an error:', err.stack);
     process.exit(1);
