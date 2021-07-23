746ca356c6627a42ee5b1a1c3d90677d8e42d221
Emil Björklund <emil.bjorklund@hdsydsvenskan.se>
2021-07-23 08:43:08 +0200

    Add a first naïve test of multi-compiler/watch case

--- test/webpack-manifest-plugin-generate.spec.js
+++ test/webpack-manifest-plugin-generate.spec.js
@@ -22,4 +22,26 @@ describe('webpackManifestPluginGenerate()', () => {
       foo: { path: '123' }
     });
   });
+
+  it('should update path when seed already contains entry (watch/multi-compiler)', function () {
+    // Represents already built file per "foo" entry
+    const seed = {
+      'myfile.js': { path: 'myfile.123.js' }
+    };
+    // New build affecting same file, but with new path for "foo"
+    const files = [
+      {
+        chunk: {
+          groupsIterable: []
+        },
+        path: 'myfile.234.js',
+        name: 'myfile.js',
+      }
+    ];
+    // @ts-ignore due to not acutally using the FileDescriptor signaruture
+    const result = webpackManifestPluginGenerate(seed, files);
+    result.should.deep.equal({
+      'myfile.js': { path: 'myfile.234.js', siblings: [] }
+    });
+  });
 });
